e2dbe86da0475b692dea3b5cad33df92b9eb19f2
easonyu0203 <breaking0203@gmail.com>
2023-03-10 01:53:59 +0800

    feat: add basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hinet_rejection_collector',
+    version='0.1.0',
+    packages=['tests', 'config_loader', 'hinet_rejection_collector'],
+    url='',
+    license='',
+    author='ethan',
+    author_email='breaking0203@gmail.com',
+    description='system for monitor rejection email and store to database'
+)
