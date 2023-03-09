df59f2495de7eca8fcaaaab6d9a8b514ff9a5b9a
easonyu0203 <breaking0203@gmail.com>
2023-03-10 01:13:03 +0800

    docs: add documentation for class in config_model
    config_model consist pydantic.BaseModel which represent config.json file structure, add docs to explain each class is corresponding which section of config.json

--- PydanticConfig/config_model.py
+++ PydanticConfig/config_model.py
@@ -2,12 +2,14 @@ from pydantic import BaseModel
 
 
 class ImapConfig(BaseModel):
+    """configuration relative to IMAP"""
     user: str
     password: str
     imap_url: str
 
 
 class Config(BaseModel):
+    """represent the configuration of the application"""
     imap: ImapConfig
 
 
